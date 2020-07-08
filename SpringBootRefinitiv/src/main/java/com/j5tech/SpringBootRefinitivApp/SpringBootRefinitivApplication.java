5424f90471c8b7ff584d21f48cc2183efec4cfd4
Jorge-Martinez-G <66711608+Jorge-Martinez-G@users.noreply.github.com>
2020-07-08 06:45:55 -0500

    Commit con Spring Tool Suite 4.7.0

---
+++ SpringBootRefinitiv/src/main/java/com/j5tech/SpringBootRefinitivApp/SpringBootRefinitivApplication.java
@@ -0,0 +1,13 @@
+package com.j5tech.SpringBootRefinitivApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRefinitivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRefinitivApplication.class, args);
+	}
+
+}
---
+++ SpringBootRefinitiv/src/test/java/com/j5tech/SpringBootRefinitivApp/SpringBootRefinitivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j5tech.SpringBootRefinitivApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRefinitivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
