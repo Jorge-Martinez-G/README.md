f783fd862710ab04e84039408b62a652a8f028bb
Jorge-Martinez-G <66711608+Jorge-Martinez-G@users.noreply.github.com>
2020-07-11 01:21:24 -0500

    User and Account entities were created.

---
+++ SpringBootRefinitiv/src/main/java/com/j5tech/SpringBootRefinitivApp/entity/Account.java
@@ -0,0 +1,102 @@
+package com.j5tech.SpringBootRefinitivApp.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+public class Account implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9080761017668158658L;
+	
+	@Id
+	@GeneratedValue (strategy=GenerationType.AUTO, generator="native")
+	@GenericGenerator(name="native", strategy="native")
+	private long Id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private String description;
+
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * Override hashCode method 
+	 */	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (Id ^ (Id >>> 32));
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		if (Id != other.Id)
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	/**
+	 * Override toString method 
+	 */	
+	@Override
+	public String toString() {
+		return "Account [Id=" + Id + ", name=" + name + ", description=" + description + "]";
+	}	
+	
+	
+}
